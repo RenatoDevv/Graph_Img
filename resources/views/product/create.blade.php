47d6a12297167645de2651eb5a091e2a112c12e9
RenatoDevv <apazaarenatogmail.com>
2023-11-09 05:43:20 -0500

    subida de archivos

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +8,49 @@
     <title>Create</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        body {
+            width: 100%;
+            height: 100vh;
+            display: flex;
+            place-items: center;
+            background: #ebebeb;
+        }
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .container form {
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            background: #fff;
+            padding: 20px 40px;
+            border-radius: 10px;
+        }
+
+        .container form input {
+            width: 100%;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Registrar Producto</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, ea.</p>
-    <form action="{{ route('product.store')}}" method="POST">
-        @csrf
-        <input type="text" name="name" placeholder="Nombres">
-        <input type="text" name="description" placeholder="Description">
-        <button type="submit">Registrar</button>
-    </form>
+    <div class="container">
+        <form action="{{ route('product.store') }}" method="POST">
+            @csrf
+            <h4 class="text-center mt-5">Registrar Servicio</h4>
+            <p>Complete los campos para registrar un nuevo servicio</p>
+            <input type="text" name="name" placeholder="Nombres" class="form-control">
+            <textarea name="description" id="" cols="30" rows="4" class="form-control"
+                placeholder="Descripcion del Servicio"></textarea>
+            <button type="submit" class="btn btn-primary">Registrar</button>
+        </form>
+    </div>
 </body>
+
 </html>
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -8,32 +8,118 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Product</title>
+    <link rel="stylesheet"
+        href="https://preview.keenthemes.com/preview/metronic/theme/html/demo1/dist/assets/plugins/global/plugins.bundle.css?v=7.2.9">
+    <style>
+        body {
+            width: 100%;
+            background: #ebebeb;
+        }
+
+        main {
+            margin: 0 auto;
+        }
+        .button-create {
+            background: #1BC5BD;
+            border-radius: 10px;
+            color: #fff;
+            font-weight: bold;
+            float: right;
+        }
+        .button-create:hover {
+            background: #1bc5bdb7;
+        }
+    </style>
 </head>
 
 <body>
-    <h1>Lista de Productos</h1>
+    <main>
+        <h4 class="text-center mt-5">Lista de Servicios Graph Evolution</h4>
 
-    <div class="container">
-        <table class="table caption-top">
-            <caption>List of users</caption>
-            <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">NOMBRE</th>
-                    <th scope="col">DESCRIPCION</th>
-                </tr>
-            </thead>
-            @foreach ($products as $product)
-            <tbody>
-                <tr>
-                    <th scope="row">{{$product->id }}</th>
-                    <td>{{$product->name}}</td>
-                    <td>{{$product->description}}</td>
-                </tr>
-            </tbody>
-            @endforeach
-        </table>
-    </div>
+        <div class="container">
+            <a href="" class="button-create btn">Nuevo Servicio</a>
+            <table class="table caption-top rounded">
+                <caption>Lista de Servicios</caption>
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">NOMBRE</th>
+                        <th scope="col">DESCRIPCION</th>
+                        <th>ACCIONES</th>
+                    </tr>
+                </thead>
+                @foreach ($products as $product)
+                    <tbody>
+                        <tr>
+                            <th scope="row">{{ $product->id }}</th>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->description }}</td>
+                            <td class="d-flex align-items-center">
+                                <a href=""><span
+                                        class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo1/dist/../src/media/svg/icons/Design/Edit.svg--><svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px"
+                                            viewBox="0 0 24 24" version="1.1">
+                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                <rect x="0" y="0" width="24" height="24" />
+                                                <path
+                                                    d="M8,17.9148182 L8,5.96685884 C8,5.56391781 8.16211443,5.17792052 8.44982609,4.89581508 L10.965708,2.42895648 C11.5426798,1.86322723 12.4640974,1.85620921 13.0496196,2.41308426 L15.5337377,4.77566479 C15.8314604,5.0588212 16,5.45170806 16,5.86258077 L16,17.9148182 C16,18.7432453 15.3284271,19.4148182 14.5,19.4148182 L9.5,19.4148182 C8.67157288,19.4148182 8,18.7432453 8,17.9148182 Z"
+                                                    fill="#3699FF" fill-rule="nonzero"
+                                                    transform="translate(12.000000, 10.707409) rotate(-135.000000) translate(-12.000000, -10.707409) " />
+                                                <rect fill="#3699FF" opacity="0.3" x="5" y="20" width="15"
+                                                    height="2" rx="1" />
+                                            </g>
+                                        </svg><!--end::Svg Icon--></span></a>
+                                <a href=""><span
+                                        class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo1/dist/../src/media/svg/icons/Home/Trash.svg--><svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px"
+                                            viewBox="0 0 24 24" version="1.1">
+                                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                                <rect x="0" y="0" width="24" height="24" />
+                                                <path
+                                                    d="M6,8 L18,8 L17.106535,19.6150447 C17.04642,20.3965405 16.3947578,21 15.6109533,21 L8.38904671,21 C7.60524225,21 6.95358004,20.3965405 6.89346498,19.6150447 L6,8 Z M8,10 L8.45438229,14.0894406 L15.5517885,14.0339036 L16,10 L8,10 Z"
+                                                    fill="#3699FF" fill-rule="nonzero" />
+                                                <path
+                                                    d="M14,4.5 L14,3.5 C14,3.22385763 13.7761424,3 13.5,3 L10.5,3 C10.2238576,3 10,3.22385763 10,3.5 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z"
+                                                    fill="#3699FF" opacity="0.3" />
+                                            </g>
+                                        </svg><!--end::Svg Icon--></span></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                @endforeach
+            </table>
+
+            {{-- <span class="svg-icon svg-icon-success">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
+                    height="24px" viewBox="0 0 24 24" version="1.1">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <rect x="0" y="0" width="24" height="24" />
+                        <path
+                            d="M6,2 L18,2 C19.6568542,2 21,3.34314575 21,5 L21,19 C21,20.6568542 19.6568542,22 18,22 L6,22 C4.34314575,22 3,20.6568542 3,19 L3,5 C3,3.34314575 4.34314575,2 6,2 Z M12,11 C13.1045695,11 14,10.1045695 14,9 C14,7.8954305 13.1045695,7 12,7 C10.8954305,7 10,7.8954305 10,9 C10,10.1045695 10.8954305,11 12,11 Z M7.00036205,16.4995035 C6.98863236,16.6619875 7.26484009,17 7.4041679,17 C11.463736,17 14.5228466,17 16.5815,17 C16.9988413,17 17.0053266,16.6221713 16.9988413,16.5 C16.8360465,13.4332455 14.6506758,12 11.9907452,12 C9.36772908,12 7.21569918,13.5165724 7.00036205,16.4995035 Z"
+                            fill="#09f" />
+                    </g>
+                </svg>
+            </span> --}}
+            {{-- <span class="label label-inline label-light-success font-weight-bold">
+                Approved
+            </span> --}}
+
+            {{-- <span
+                class="svg-icon svg-icon-primary svg-icon-2x"><!--begin::Svg Icon | path:/var/www/preview.keenthemes.com/metronic/releases/2021-05-14-112058/theme/html/demo1/dist/../src/media/svg/icons/Design/Component.svg--><svg
+                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px"
+                    height="24px" viewBox="0 0 24 24" version="1.1">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <rect x="0" y="0" width="24" height="24" />
+                        <path
+                            d="M12.7442084,3.27882877 L19.2473374,6.9949025 C19.7146999,7.26196679 20.003129,7.75898194 20.003129,8.29726722 L20.003129,15.7027328 C20.003129,16.2410181 19.7146999,16.7380332 19.2473374,17.0050975 L12.7442084,20.7211712 C12.2830594,20.9846849 11.7169406,20.9846849 11.2557916,20.7211712 L4.75266256,17.0050975 C4.28530007,16.7380332 3.99687097,16.2410181 3.99687097,15.7027328 L3.99687097,8.29726722 C3.99687097,7.75898194 4.28530007,7.26196679 4.75266256,6.9949025 L11.2557916,3.27882877 C11.7169406,3.01531506 12.2830594,3.01531506 12.7442084,3.27882877 Z M12,14.5 C13.3807119,14.5 14.5,13.3807119 14.5,12 C14.5,10.6192881 13.3807119,9.5 12,9.5 C10.6192881,9.5 9.5,10.6192881 9.5,12 C9.5,13.3807119 10.6192881,14.5 12,14.5 Z"
+                            fill="#1BC5BD" />
+                    </g>
+                </svg><!--end::Svg Icon--></span> --}}
+        </div>
+
+    </main>
 </body>
 
 </html>
